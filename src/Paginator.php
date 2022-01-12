d296d164a9d529ac89c8be36bd107310159b23c7
Robert Teschmacher <r.teschmacher@gmail.com>
2022-01-12 19:46:30 +0000

    Created Paginator, Array and Traversable Tests

---
+++ src/Paginator.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Robtesch\IterablePaginator;
+
+class Paginator
+{
+    private array $items;
+    private int $total;
+    private int $perPage;
+    private int $currentPage;
+
+    public function __construct(iterable $items, int $total, int $perPage, int $currentPage = 1)
+    {
+        if (is_array($items)) {
+            $this->items = $items;
+        } elseif ($items instanceof \Traversable) {
+            $this->items = iterator_to_array($items);
+        } else {
+            throw new \InvalidArgumentException('expected array or Traversable for items.');
+        }
+        $this->total = $total;
+        $this->perPage = $perPage;
+        $this->currentPage = $currentPage;
+    }
+
+    public function items(): iterable
+    {
+        return $this->items;
+    }
+
+    public function currentPage(): int
+    {
+        return $this->currentPage;
+    }
+
+    public function pages(): int
+    {
+        return (int)ceil($this->total / $this->perPage);
+    }
+
+    public function total(): int
+    {
+        return $this->total;
+    }
+
+    public function totalElementsOnCurrentPage(): int
+    {
+        return count($this->paginate());
+    }
+
+    private function getOffset(): int
+    {
+        return max(0, ($this->currentPage - 1) * $this->perPage);
+    }
+
+    public function paginate(?int $page = null)
+    {
+        if (is_int($page)) {
+            $this->currentPage = $page;
+        }
+
+        if($this->currentPage > $this->pages()) {
+            $this->currentPage = $this->pages();
+        }
+
+        return array_slice($this->items, $this->getOffset(), $this->perPage, false);
+    }
+
+    public function toArray(): array
+    {
+        return [
+            "current_page" => $this->currentPage,
+            "data"         => $this->paginate(),
+            "from"         => $this->first(),
+            "to"           => $this->last(),
+            "last_page"    => $this->pages(),
+            "per_page"     => $this->perPage,
+            "total"        => $this->total,
+        ];
+    }
+
+    private function first(): ?int
+    {
+        return count($this->items) > 0 ? ($this->currentPage - 1) * $this->perPage + 1 : null;
+    }
+
+    public function last(): ?int
+    {
+        return count($this->items) > 0 ? $this->first() + count($this->paginate()) - 1 : null;
+    }
+}
---
+++ tests/PaginatesArrayTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use Robtesch\IterablePaginator\Paginator;
+
+class PaginatesArrayTest extends TestCase
+{
+    public function testPaginates100Items(): void
+    {
+        $array = range(1, 100);
+
+        $pagination = (new Paginator($array, count($array), 15))->paginate();
+
+        $this->assertCount(15, $pagination);
+    }
+
+    public function testPaginates100ItemsPage2(): void
+    {
+        $array = range(1, 100);
+
+        $pagination = (new Paginator($array, count($array), 15))->paginate(2);
+
+        $this->assertCount(15, $pagination);
+
+        for ($i = 0; $i < 15; $i++) {
+            $this->assertEquals($i + 16, $pagination[$i]);
+        }
+    }
+
+    public function testReturnsSaneResultsForNegativePageNumber(): void
+    {
+        $array = range(1, 100);
+
+        $pagination = (new Paginator($array, count($array), 15))->paginate(-1);
+
+        $this->assertCount(15, $pagination);
+    }
+
+    public function testReturnsSaneResultsForMassivePageNumber(): void
+    {
+        $array = range(1, 100);
+
+        $pagination = (new Paginator($array, count($array), 15))->paginate(100);
+
+        $this->assertGreaterThan(0, $pagination);
+    }
+
+    public function testReturnsCorrectArrayRepresentation(): void
+    {
+        $all = ['chimp', 'turtle', 'chicken', 'pig', 'dog', 'frog', 'ostrich', 'cow', 'elephant', 'giraffe'];
+
+        $pagination = (new Paginator($all, count($all), 3, 2));
+
+        $expected = [
+            "current_page" => 2,
+            "data"         => ['pig', 'dog', 'frog'],
+            "from"         => 4,
+            "to"           => 6,
+            "last_page"    => 4,
+            "per_page"     => 3,
+            "total"        => 10,
+        ];
+
+        $this->assertEquals($expected, $pagination->toArray());
+    }
+}
